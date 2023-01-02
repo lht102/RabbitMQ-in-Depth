c7d69ec1101b54e43b18983db81f9cb36db0b14c
Tony Lau <lht102.p@gmail.com>
2023-01-02 18:58:38 +0800

    add example 6.7

---
+++ Examples/go/6.7-A-Consistent-Hashing-Exchange-that-Routes-on-a-Routing-Key/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Panicf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Panicf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	if err := ch.ExchangeDeclare(
+		"image-storage",
+		"x-consistent-hash",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		log.Panicf("Failed to declare an exchange: %v", err)
+	}
+}
